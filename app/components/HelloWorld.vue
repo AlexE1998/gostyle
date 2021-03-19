2dc9668b21136e11ad58e4a3e3a749a97e132262
AlexE1998 <aernault44@gmail.com>
2021-03-19 13:31:59 +0100

    first commit

---
+++ app/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+    <Page class="page">
+        <ActionBar title="Connexion" flat="true" />
+        <BottomNavigation>
+            <TabContentItem>
+                <GridLayout rows="auto,auto,auto,auto,auto,auto,auto">
+                    <Label row="1" class="loginForm resizeTop "
+                        text="Pseudo :" />
+                    <TextField row="2" class="loginForm resizeBottom"
+                        v-model="usernameLoginValue" hint="Ex : Dupont44 " />
+                    <Label row="3" class="loginForm" text="Mot de passe :" />
+                    <TextField row="4" secure="true"
+                        class="loginForm resizeBottom"
+                        v-model="passwordLoginValue" hint="Ex : 123AbCd" />
+                    <Button row="5" class="buttonLogin" text="Connexion"
+                        @tap="onLoginTap" />
+                    <Button row="6" class="buttonLogin" text="Nouveau compte"
+                        @tap="onRegisterTap" />
+                </GridLayout>
+            </TabContentItem>
+        </BottomNavigation>
+
+    </Page>
+</template>
+
+<script>
+    import Register from "./Register";
+    export default {
+        methods: {
+            onButtonTap() {
+                console.log("Button was pressed");
+            },
+
+            onItemTap: function(args) {
+                console.log("Item with index: " + args.index + " tapped");
+            },
+            onRegisterTap() {
+                this.$navigateTo(Register);
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    .loginForm {
+        font-size: 16;
+        margin-left: 50;
+        margin-right: 50;
+    }
+
+    .resizeTop {
+        margin-top: 50;
+    }
+
+    .resizeBottom {
+        margin-bottom: 50;
+    }
+
+    .buttonLogin {
+        background-color: #43378E;
+    }
+</style>
---
+++ app/components/Register.vue
@@ -0,0 +1,50 @@
+<template>
+    <Page class="page">
+        <ActionBar title="Connexion" flat="true" />
+        <BottomNavigation>
+            <TabContentItem>
+                <GridLayout
+                    rows="auto,auto,auto,auto,auto,auto,auto,auto,auto,auto,auto,auto,auto,auto,auto">
+                    <Label row="1" class="loginForm resizeTop "
+                        text="Nom :" />
+                    <TextField row="2" class="loginForm resizeBottom"
+                        v-model="nameRegisterValue" hint="Ex : Dupont " />
+
+                    <Label row="3" class="loginForm" text="Prénom :" />
+                    <TextField row="4" class="loginForm resizeBottom"
+                        v-model="usernameRegisterValue" hint="Ex : Alain" />
+
+                    <Label row="5" class="loginForm" text="E-mail :" />
+                    <TextField row="6" class="loginForm resizeBottom"
+                        v-model="emailRegisterValue"
+                        hint="Ex : mail@mail.com" />
+
+                    <Label row="7" class="loginForm" text="Pseudo :" />
+                    <TextField row="8" class="loginForm resizeBottom"
+                        v-model="pseudoRegisterValue" hint="Ex : Dupont44" />
+
+                    <Label row="9" class="loginForm" text="Mot de passe :" />
+                    <TextField row="10" secure="true"
+                        class="loginForm resizeBottom"
+                        v-model="usernameRegisterValue" hint="Ex : 123AbCd" />
+
+                    <Button row="11" class="buttonLogin" text="Valider"
+                        @tap="onCreateAccount" />
+
+                </GridLayout>
+            </TabContentItem>
+        </BottomNavigation>
+
+    </Page>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {};
+        }
+    };
+</script>
+
+<style>
+</style>
